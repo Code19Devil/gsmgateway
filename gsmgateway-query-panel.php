e63cb42d7cd57a6f4195ab3a684d327f71417762
Code19Devil <gunjanmishra305@gmail.com>
2023-11-24 15:38:24 +0530

    asfera

--- gsmgateway-query-panel.php
+++ gsmgateway-query-panel.php
@@ -16,22 +16,24 @@
   <link rel="stylesheet" href="items/basic/assets/css/bootstrap-4.5.0.min.css">
   <link rel="stylesheet" href="items/basic/assets/css/default.css">
   <link rel="stylesheet" href="items/basic/assets/css/ownstyle.css">
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
   <style media="screen">
-  .show-data {
-width: 100%;
-height: 650px;
-white-space: nowrap;
-display: block;
-overflow: auto;
-}
-
+    .show-data {
+      width: 100%;
+      height: 650px;
+      white-space: nowrap;
+      display: block;
+      overflow: auto;
+    }
   </style>
 </head>
+
 <body>
 
   <header class="header-area">
@@ -40,7 +42,8 @@ overflow: auto;
         <a href="index.html">
           <img src="items/own_images/logoes.png" alt="Gsmgateway" style='width: 10em;height: 3em;margin-top: -0.5em;'>
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent" style="background-color: #095ae6;">
@@ -49,7 +52,8 @@ overflow: auto;
               <a class="nav-link " href="index.html">Home</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="openvox/openvox.html" role="button" aria-expanded="false">OpenVox</a>
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="openvox/openvox.html" role="button"
+                aria-expanded="false">OpenVox</a>
               <ul class="dropdown-menu dropdown_own_style" style="background-color: #095ae6;">
                 <li><a class="dropdown-item" href="openvox/SWG-Series-Gateways.html">SWG Series Gateways</a></li>
                 <li><a class="dropdown-item" href="openvox/WCDMA-VOIP-Gateways.html">WCDMA VOIP Gateways</a></li>
@@ -63,16 +67,20 @@ overflow: auto;
               <a class="nav-link" href="synway/synway_products.html">Synway</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="dinstar/dinstar.html" role="button" aria-expanded="false">Dinstar</a>
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="dinstar/dinstar.html" role="button"
+                aria-expanded="false">Dinstar</a>
               <ul class="dropdown-menu dropdown_own_style" style="background-color: #095ae6;">
                 <li><a class="dropdown-item" href="dinstar/Digital-VoIP-Gateway.html">Digital-VoIP-Gateway</a></li>
                 <li><a class="dropdown-item" href="dinstar/GSM-3G-4G-VoIP-Gateway.html">GSM-3G-4G-VoIP-Gateway</a></li>
                 <li class="hover-me nav-item"><a class="dropdown-item" href="#">Analog-VoIP-Gateway</a>
                   <div class="sub-menu-2">
                     <ul>
-                      <li><a class="dropdown-item" href="dinstar/Analog-voip-gateway/fxo-gateway.html">FXO-&nbsp;Gateways&nbsp;</a></li>
-                      <li><a class="dropdown-item" href="dinstar/Analog-voip-gateway/fxs-gateway.html">FXS-Gateways</a></li>
-                      <li><a class="dropdown-item" href="dinstar/Analog-voip-gateway/hybrid-voip-gateway.html">Hybrid-Gateways</hr></a></li>
+                      <li><a class="dropdown-item"
+                          href="dinstar/Analog-voip-gateway/fxo-gateway.html">FXO-&nbsp;Gateways&nbsp;</a></li>
+                      <li><a class="dropdown-item" href="dinstar/Analog-voip-gateway/fxs-gateway.html">FXS-Gateways</a>
+                      </li>
+                      <li><a class="dropdown-item"
+                          href="dinstar/Analog-voip-gateway/hybrid-voip-gateway.html">Hybrid-Gateways</hr></a></li>
                     </ul>
                   </div>
                 </li>
@@ -125,39 +133,54 @@ overflow: auto;
                 <th width="55%">Message</th>
               </tr>
               <?php
-            require 'dbconn.php';
-            $sql="SELECT DATE(submitted_on) as date,name,email,mobile,message,id from call_center_query";
-            $result = mysqli_query($link, $sql);
-           ?>
-              <?php  while($row=mysqli_fetch_array($result)): ?>
-              <tr class="text-left">
-                <td><?php print($row['date'])?></td>
-                <td><?php print($row['name'])?></td>
-                <td><?php print($row['email']."<br>".$row['mobile'])?></td>
-                <td><?php print($row['message'])?></td>
-              </tr>
-            <?php endwhile;mysqli_close($link); ?>
+              require 'dbconn.php';
+              $sql = "SELECT DATE(submitted_on) as date,name,email,mobile,message,id from call_center_query";
+              $result = mysqli_query($link, $sql);
+              ?>
+              <?php while ($row = mysqli_fetch_array($result)): ?>
+                <tr class="text-left">
+                  <td>
+                    <?php print($row['date']) ?>
+                  </td>
+                  <td>
+                    <?php print($row['name']) ?>
+                  </td>
+                  <td>
+                    <?php print($row['email'] . "<br>" . $row['mobile']) ?>
+                  </td>
+                  <td>
+                    <?php print($row['message']) ?>
+                  </td>
+                </tr>
+              <?php endwhile;
+              mysqli_close($link); ?>
             </table>
           </div>
         </div>
+      </div>
     </div>
-  </div>
   </section>
-<script src="items/basic/assets/js/vendor/modernizr-3.7.1.min.js"></script>
-<script src="items/basic/assets/js/jquery-3.5.1-min.js"></script>
-<script src="items/basic/assets/js/popper.min.js"></script>
-<script src="items/basic/assets/js/bootstrap-4.5.0.min.js"></script>
-<script src="items/basic/assets/js/plugins.js"></script>
-<script src="items/basic/assets/js/waypoints.min.js"></script>
-<script src="items/basic/assets/js/jquery.counterup.min.js"></script>
-<script src="items/basic/assets/js/jquery.magnific-popup.min.js"></script>
-<script src="items/basic/assets/js/jquery.easing.min.js"></script>
-<script src="items/basic/assets/js/scrolling-nav.js"></script>
-<script src="items/basic/assets/js/wow.min.js"></script>
-<script src="items/basic/assets/js/particles.min.js"></script>
-<script src="items/basic/assets/js/main.js"></script>
-<script defer src="items/basic/assets/js/beacon.min.js" data-cf-beacon='{"rayId":"68a7b99d88d62bc8","version":"2021.8.1","r":1,"token":"e30b4e5ca0154d28b74c094697cafe5d","si":10}'></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  <script src="items/basic/assets/js/vendor/modernizr-3.7.1.min.js"></script>
+  <script src="items/basic/assets/js/jquery-3.5.1-min.js"></script>
+  <script src="items/basic/assets/js/popper.min.js"></script>
+  <script src="items/basic/assets/js/bootstrap-4.5.0.min.js"></script>
+  <script src="items/basic/assets/js/plugins.js"></script>
+  <script src="items/basic/assets/js/waypoints.min.js"></script>
+  <script src="items/basic/assets/js/jquery.counterup.min.js"></script>
+  <script src="items/basic/assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="items/basic/assets/js/jquery.easing.min.js"></script>
+  <script src="items/basic/assets/js/scrolling-nav.js"></script>
+  <script src="items/basic/assets/js/wow.min.js"></script>
+  <script src="items/basic/assets/js/particles.min.js"></script>
+  <script src="items/basic/assets/js/main.js"></script>
+  <script defer src="items/basic/assets/js/beacon.min.js"
+    data-cf-beacon='{"rayId":"68a7b99d88d62bc8","version":"2021.8.1","r":1,"token":"e30b4e5ca0154d28b74c094697cafe5d","si":10}'></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+    crossorigin="anonymous"></script>
 </body>
+
 </html>
